2e5b4ba20aa8f11ca9d5d4cc1e005f4295613e65
Hoyeoun Byun <jay08111@naver.com>
2021-10-15 19:09:28 +0900

    add form

--- map/map.js
+++ map/map.js
@@ -1,3 +1,12 @@
+"use strict";
+const menu = document.querySelector(".input--menu");
+const address = document.querySelector(".input--address");
 document.querySelector(".submit").addEventListener("click", () => {
-  window.alert("주문완료되었습니다!");
+  if (!menu.value) {
+    window.alert("메뉴를 입력하세요!");
+  } else if (!address.value) {
+    window.alert("주소를 입력하세요!");
+  } else {
+    window.alert("주문완료되었습니다!");
+  }
 });

f4f85a4f270a314a236955faa8d807552be7d4d3
Hoyeoun Byun <jay08111@naver.com>
2021-10-19 12:24:35 +0900

    add menu

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-class Pasta {
-  async getPasta() {
-    try {
-      let result = await fetch("pasta.json");
-      let data = await result.json();
-      console.log(data);
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
+let sliderImages = document.querySelectorAll(".slide");
+const arrowLeft = document.querySelector(".arrow-left");
+const arrowRight = document.querySelector(".arrow-right");
+let current = 0;
+
+const reset = () => {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = "none";
   }
-}
-document.addEventListener("DOMContentLoaded", () => {
-  const pasta = new Pasta();
-  pasta.getPasta();
+};
+const startSlide = () => {
+  reset();
+  sliderImages[0].style.display = "block";
+};
+const slideLeft = () => {
+  reset();
+  sliderImages[current - 1].style.display = "block";
+  current--;
+};
+const slideRight = () => {
+  reset();
+  sliderImages[current + 1].style.display = "block";
+  current++;
+};
+arrowLeft.addEventListener("click", function () {
+  if (current === 0) {
+    current = sliderImages.length;
+  }
+  slideLeft();
 });
+arrowRight.addEventListener("click", function () {
+  if (current === sliderImages.length - 1) {
+    current = -1;
+  }
+  slideRight();
+});
+startSlide();
---
+++ menu/menu.js
@@ -0,0 +1,32 @@
+const imageContainer = document.querySelector(".img-container");
+
+class Pasta {
+  async getPasta() {
+    try {
+      let result = await fetch("../pasta.json");
+      let data = await result.json();
+      let items = data.items;
+      console.log(items);
+      return items;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  displayImage(item) {
+    let result = "";
+    item.map((pasta) => {
+      result += `<div>
+      <img src=${pasta.image} alt=${pasta.name} class="menu-image" />
+      <h4>${pasta.price}$</h4>
+      <button>장바구니에 추가</button>
+      </div>`;
+    });
+    imageContainer.innerHTML = result;
+  }
+}
+document.addEventListener("DOMContentLoaded", () => {
+  const pasta = new Pasta();
+  pasta.getPasta().then((item) => {
+    pasta.displayImage(item);
+  });
+});

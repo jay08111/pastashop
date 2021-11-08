c7bf229ae1a91f08ae516c4e87d4c90ec6815dca
Hoyeoun Byun <jay08111@naver.com>
2021-11-08 14:05:14 +0900

    add cart

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 "use strict";
 const foodContainer = document.querySelector(".food-container");
+const cartIcon = document.querySelector(".cart-icon");
+const cartOverLay = document.querySelector(".cart-overlay");
+const closeCart = document.querySelector(".close-cart");
+const cart = document.querySelector(".cart");
 
+let buttonDOM = [];
+let cartDOM = [];
+//functions
 const getPasta = async () => {
   try {
     const res = await fetch("pasta.json");
@@ -27,6 +34,31 @@ const displayItem = (item) => {
   });
   foodContainer.innerHTML = result;
 };
+const getBagBtns = () => {
+  const buttons = [...document.querySelectorAll(".food-bagBtn")];
+  buttonDOM = buttons;
+  buttons.map((btn) => {
+    let id = btn.dataset.id;
+    // let inCart = cart.find((item) => item.id === id);
+    btn.addEventListener("click", () => {
+      cartOverLay.classList.add("visible");
+      cart.classList.add("showCart");
+    });
+  });
+};
+//Event Listeners
 document.addEventListener("DOMContentLoaded", () => {
-  getPasta().then((pasta) => displayItem(pasta));
+  getPasta()
+    .then((pasta) => displayItem(pasta))
+    .then(() => {
+      getBagBtns();
+    });
+});
+cartIcon.addEventListener("click", () => {
+  cartOverLay.classList.add("visible");
+  cart.classList.add("showCart");
+});
+closeCart.addEventListener("click", () => {
+  cartOverLay.classList.remove("visible");
+  cart.classList.remove("showCart");
 });
